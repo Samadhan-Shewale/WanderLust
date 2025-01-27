70ed2f3047f2def0ee22fed70a8f3875f5eefab3
Samadhan-Shewale <shewalesamadhan512@gmail.com>
2025-01-28 01:31:53 +0530

    restructure the code using express Router

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const app = express();
 const path = require("path");
 const methodOverride  =  require("method-override"); 
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");   // custom express-error handler class
-const {listingSchema, reviewSchema } = require("./schema.js");          // schema validator surver side ( Joi )
-const Review = require("./models/review.js");              
+
+const listings = require("./Router/listing.js");    // contain the methods of listings
+const reviews = require("./Router/review.js");      // // contain the methods of reviews
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -30,110 +30,11 @@ async function main(){
    await mongoose.connect(MONGO_URL);
 }
 
-// converting validation into middleware
-const validateListing = (req,res, next ) =>{
-    let {error } = listingSchema.validate(req.body);
-    if( error ){
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg );
-    }else{
-        next();
-    }
-}
-
-const validateReview = (req,res, next ) =>{
-    let {error } = reviewSchema.validate(req.body);
-    if( error ){
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg );
-    }else{
-        next();
-    }
-}
-
-// index route 
-app.get("/listings", wrapAsync ( async (req,res) =>{
-    const allListing = await Listing.find();
-    res.render("./listings/index.ejs",{allListing });
-}))
 
 
-// Create route 
-app.get("/listings/new",(req,res) => {
-    res.render("./listings/new.ejs");
-})
+app.use("/listings", listings);
+app.use("/listings/:id/reviews",reviews );
 
-// show route
-app.get("/listings/:id", wrapAsync (async (req,res) =>{
-    const { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("./listings/show.ejs", {listing});
-}));
-
-// new route 
-app.post("/listings", validateListing , wrapAsync (async (req,res, next)  => {
-        
-        
-        const newListing = new Listing( req.body.listing );
-        await newListing.save();
-        res.redirect("/listings");
-}))
-
-// edit and update route
-app.get("/listings/:id/edit", wrapAsync ( async(req, res) =>{
-    const {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("./listings/edit.ejs",{ listing});
-}))
-
-// update route
-app.put("/listings/:id", validateListing, wrapAsync ( async (req, res) =>{
-    // if(! req.body.listing ){
-    //     throw new ExpressError(400, "Send valid data for listing ");
-    // }
-    const {id} = req.params;
-    await Listing.findByIdAndUpdate( id, {...req.body.listing })
-    res.redirect(`/listings/${id}`);
-
-}))
-
-// delete route  
-app.delete("/listings/:id", wrapAsync ( async (req, res) =>{
-   const {id} = req.params ;
-   
-   // approach 1 ( deleting )
-//    let listing = await Listing.findById(id);
-//    console.log(listing.reviews);
-//     await Review.deleteMany({ _id: { $in : listing.reviews }});
-
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-}))
-
-// Reviews 
-//  Post Route 
-app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req,res) =>{
-    const listing = await Listing.findById(req.params.id);
-    const newReview = new Review(req.body.review);
-    
-    listing.reviews.push(newReview );
-
-    await newReview.save();
-    await listing.save();
-
-   res.redirect(`/listings/${listing._id}`)
-}));
-
-// reviews delete route
-app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req,res) =>{
-    let { id , reviewId } = req.params;
-    
-    await Listing.findByIdAndUpdate( id, {$pull : { reviews : reviewId }});
-    await Review.findByIdAndDelete(reviewId);
-
-    res.redirect(`/listings/${id}`);
-
-}));
 
 
 app.get("/", (req,res) =>{
@@ -148,11 +49,9 @@ app.all("*", (req,res,next) =>{
 app.use((err,req,res,next)=>{
    let {status = 500, message = "Something went wrong!" } = err;
    res.status(status).render("error.ejs", {message});
-    //   res.status(status).send(message);
-})
+
+});
 
 app.listen( 8080, () =>{
     console.log("Surver is listening on port 8080");
 })
-
-
